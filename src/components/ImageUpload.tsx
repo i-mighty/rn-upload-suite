674651d0aea0bd11437b9cf640c9d662e905156d
Adegboye Josiah <josadegboye@gmail.com>
2023-03-14 21:07:13 +0100

    feat: small bugs fixes

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,24 +1,36 @@
-import React, { forwardRef, useImperativeHandle, ReactNode, useState } from "react"
-import * as ImagePicker from "expo-image-picker"
-import { Image, ImageStyle, View, ViewStyle, TouchableOpacity } from "react-native"
-import { ImagePickerAsset, ImagePickerOptions } from "expo-image-picker"
-import { widthPercentageToDP as wp } from "react-native-responsive-screen"
+import React, {
+  forwardRef,
+  useImperativeHandle,
+  ReactNode,
+  useState,
+  useCallback,
+} from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import {
+  Image,
+  ImageStyle,
+  View,
+  ViewStyle,
+  TouchableOpacity,
+} from 'react-native';
+import type { ImagePickerAsset, ImagePickerOptions } from 'expo-image-picker';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 export interface Asset extends ImagePickerAsset {
-  blob: Blob
+  blob: Blob;
 }
 
 type UploadStatus =
-  | "none"
-  | "upload-failed"
-  | "single-upload-success"
-  | "multiple-upload-success"
-  | "single-upload-failed"
-  | "multiple-upload-failed"
+  | 'none'
+  | 'upload-failed'
+  | 'single-upload-success'
+  | 'multiple-upload-success'
+  | 'single-upload-failed'
+  | 'multiple-upload-failed';
 
 export interface UploadAsset {
-  state: "success" | "failed" | unknown
-  asset: Asset
+  state: 'queued' | 'success' | 'failed' | unknown;
+  asset: Asset;
 }
 
 export interface ImageUploadProps extends ImagePickerOptions {
@@ -27,47 +39,47 @@ export interface ImageUploadProps extends ImagePickerOptions {
    * Opens the camera or the library based on the mode passed
    *  @accepts ReactNode and ReactNode[]
    */
-  children?: ReactNode | ReactNode[]
+  children?: ReactNode | ReactNode[];
   /**
    * Determines if the image is selected from the camera or from the image library.
    * @accepts "camera" | "library"
    */
-  mode?: "camera" | "library"
+  mode?: 'camera' | 'library';
   /**
    * Function to handle the image upload to the storage
    * @param assets can be a single asset or an assets
    * @returns an upload asset (Asset appended with upload status) or an upload asset array.
    */
   uploadAction: <T extends Asset | Asset[]>(
-    assets: T,
-  ) => Promise<T extends Asset ? UploadAsset : UploadAsset[]>
+    assets: T
+  ) => Promise<T extends Asset ? UploadAsset : UploadAsset[]>;
   /**
    * Action to perform if the picker or camera fails and no image asset is selected
    * @returns void
    */
-  handleImageSelectionError?: () => void
+  handleImageSelectionError?: () => void;
   /**
    * Function to handle the error on uploading the files.
    * @param assets can be a single upload asset (Asset appended with status) or an array of upload assets
    * @returns
    */
-  handleImageUploadError?: (assets: UploadAsset | UploadAsset[]) => void
+  handleImageUploadError?: (assets: UploadAsset | UploadAsset[]) => void;
 }
 
 export interface ImageUploadRef {
   /**
    * Default status of the file upload process
    */
-  uploadStatus: UploadStatus
+  uploadStatus: UploadStatus;
   /**
    * Indicates wether files are actively being (network) uploaded at the moment
    */
-  uploading: boolean
+  uploading: boolean;
   /**
    * Triggers the start of the image selection process. Opens the camera or the library based on the mode passed
    * @returns void
    */
-  imageSelectionAction: () => Promise<void>
+  imageSelectionAction: () => Promise<void>;
 }
 
 export const ImageUpload = forwardRef<ImageUploadRef, ImageUploadProps>(
@@ -75,73 +87,83 @@ export const ImageUpload = forwardRef<ImageUploadRef, ImageUploadProps>(
     {
       children,
       uploadAction,
-      mode = "library",
+      mode = 'library',
       handleImageSelectionError,
       handleImageUploadError,
       ...pickerOptions
     },
-    ref,
+    ref
   ) => {
-    const [uploadStatus, setUploadStatus] = useState<UploadStatus>("none")
-    const [uploading, setUploading] = useState(false)
-    const pickerImage = require("../../assets/file-picker-icons/upload-image.png")
-    const cameraImage = require("../../assets/file-picker-icons/camera-upload.png")
+    const [uploadStatus, setUploadStatus] = useState<UploadStatus>('none');
+    const [uploading, setUploading] = useState(false);
+    const pickerImage = require('../../assets/file-picker-icons/upload-image.png');
+    const cameraImage = require('../../assets/file-picker-icons/camera-upload.png');
 
-    const openImagePickerAsync = async () => {
-      let pickerResult: ImagePicker.ImagePickerResult | null = null
+    const openImagePickerAsync = useCallback(async () => {
+      let pickerResult: ImagePicker.ImagePickerResult | null = null;
       try {
-        if (mode == "library") {
+        if (mode === 'library') {
           pickerResult = await ImagePicker.launchImageLibraryAsync({
             ...pickerOptions,
-          })
-        } else if (mode == "camera") {
+          });
+        } else if (mode === 'camera') {
           pickerResult = await ImagePicker.launchCameraAsync({
             ...pickerOptions,
-          })
+          });
         }
 
-        if (pickerResult && !pickerResult.canceled) {
-          setUploading(true)
-          const { assets } = pickerResult
+        if (
+          pickerResult &&
+          !pickerResult.canceled &&
+          pickerResult.assets.length
+        ) {
+          setUploading(true);
+          const { assets } = pickerResult;
           const assetsWithBlob = await Promise.all(
             assets.map(async (asset) => {
               return {
                 ...asset,
                 blob: await (await fetch(asset.uri)).blob(),
-              }
-            }),
-          )
+              };
+            })
+          );
 
-          if (!pickerOptions.allowsMultipleSelection) {
-            const asset = assetsWithBlob[0]
-            const uploadRes = await uploadAction(asset)
-            if (uploadRes.state == "success") {
-              setUploadStatus("single-upload-success")
-              setUploading(false)
+          if (!pickerOptions.allowsMultipleSelection && !!assetsWithBlob[0]) {
+            const asset = assetsWithBlob[0];
+            const uploadRes = await uploadAction(asset);
+            if (uploadRes.state === 'success') {
+              setUploadStatus('single-upload-success');
+              setUploading(false);
             } else {
-              setUploadStatus("single-upload-failed")
-              handleImageUploadError && handleImageUploadError(uploadRes)
-              setUploading(false)
+              setUploadStatus('single-upload-failed');
+              handleImageUploadError && handleImageUploadError(uploadRes);
+              setUploading(false);
             }
           }
           if (pickerOptions.allowsMultipleSelection) {
-            const uploadRes = await uploadAction(assetsWithBlob)
-            if (uploadRes.every((val) => val.state == "success")) {
-              setUploadStatus("multiple-upload-success")
-              setUploading(false)
+            const uploadRes = await uploadAction(assetsWithBlob);
+            if (uploadRes.every((val) => val.state === 'success')) {
+              setUploadStatus('multiple-upload-success');
+              setUploading(false);
             } else {
-              setUploadStatus("single-upload-failed")
-              handleImageUploadError && handleImageUploadError(uploadRes)
-              setUploading(false)
+              setUploadStatus('single-upload-failed');
+              handleImageUploadError && handleImageUploadError(uploadRes);
+              setUploading(false);
             }
           }
         } else {
-          handleImageSelectionError && handleImageSelectionError()
+          handleImageSelectionError && handleImageSelectionError();
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
+    }, [
+      handleImageSelectionError,
+      handleImageUploadError,
+      mode,
+      pickerOptions,
+      uploadAction,
+    ]);
     useImperativeHandle(
       ref,
       () => ({
@@ -149,29 +171,29 @@ export const ImageUpload = forwardRef<ImageUploadRef, ImageUploadProps>(
         uploading,
         imageSelectionAction: openImagePickerAsync,
       }),
-      [],
-    )
+      [uploadStatus, uploading, openImagePickerAsync]
+    );
     return (
       <TouchableOpacity onPress={() => openImagePickerAsync()}>
         {children || (
           <View style={$baseView}>
             <Image
               style={$imageIcon}
-              source={mode == "camera" ? cameraImage : pickerImage}
+              source={mode === 'camera' ? cameraImage : pickerImage}
               resizeMode="contain"
             />
           </View>
         )}
       </TouchableOpacity>
-    )
-  },
-)
+    );
+  }
+);
 
 const $baseView: ViewStyle = {
-  justifyContent: "center",
-  alignItems: "center",
-}
+  justifyContent: 'center',
+  alignItems: 'center',
+};
 const $imageIcon: ImageStyle = {
   width: wp(10),
   height: wp(10),
-}
+};
